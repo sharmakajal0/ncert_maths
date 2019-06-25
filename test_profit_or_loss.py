46e7d8b7d579243d40f5f64ddfc6bcaa38953d5b
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-06-25 21:53:00 +0530

    Test for p

---
+++ test_profit_or_loss.py
@@ -0,0 +1,18 @@
+from profit_or_loss import Profit_or_loss
+from sys import argv
+
+def main(args):
+    cp = int(args[1])
+    sp = int(args[2])
+    
+    print("The cost price and selling price are %d and %d" % (cp, sp))
+
+    myprofit = Profit_or_loss(cp, sp)
+    if sp > cp:
+        print("The profit percentage is ", myprofit.profit())
+    elif sp < cp:
+        print("The loss percentage is ", myprofit.loss())
+    else:
+        print("The profit is ", myprofit.neitherprofitnorloss())
+if __name__ == "__main__":
+    main(argv)
